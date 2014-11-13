5e68056cb14c1336bd7b1614ec544f63b397ab3d
Anderson Braz <anderson.braz@gmail.com>
2014-11-13 18:26:30 -0200

    Bootstrap 3.3.1

--- bootstrap/src/main/java/hitz/virtuozo/ui/PanelGroup.java
+++ bootstrap/src/main/java/hitz/virtuozo/ui/PanelGroup.java
@@ -1,6 +1,8 @@
 package hitz.virtuozo.ui;
 
 import hitz.virtuozo.ui.api.ActivationEvent;
+import hitz.virtuozo.ui.api.HasIcon;
+import hitz.virtuozo.ui.api.Icon;
 import hitz.virtuozo.ui.api.ActivationEvent.ActivationHandler;
 import hitz.virtuozo.ui.api.DeactivationEvent;
 import hitz.virtuozo.ui.api.DeactivationEvent.DeactivationHandler;
@@ -11,6 +13,7 @@ import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.DoubleClickHandler;
+import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
 
 public class PanelGroup extends Widget<PanelGroup> {
   private ActivationHelper helper = new ActivationHelper();
@@ -20,7 +23,7 @@ public class PanelGroup extends Widget<PanelGroup> {
     this.css("panel-group");
   }
 
-  public Panel addPanel(String title) {
+  public CollapsePanel addPanel(String title) {
     CollapsePanel panel = new CollapsePanel(title);
     if(!this.hasChildren()){
       panel.activate();
@@ -30,7 +33,7 @@ public class PanelGroup extends Widget<PanelGroup> {
     return panel;
   }
 
-  class CollapsePanel extends Panel implements HasActivation<CollapsePanel> {
+  public class CollapsePanel extends Panel implements HasActivation<CollapsePanel>, HasIcon<CollapsePanel> {
     private Tag<DivElement> collapse = Tag.asDiv().css("panel-collapse", "collapse");
 
     private Tag<AnchorElement> anchor = Tag.asAnchor().css("collapsed");
@@ -38,7 +41,26 @@ public class PanelGroup extends Widget<PanelGroup> {
     public CollapsePanel(String title) {
       this.header().heading().addChild(this.anchor.html(title));
       this.addChild(this.collapse);
-      this.collapse.add(this.body().detach()).add(this.footer().detach().hide());
+      this.collapse.add(this.body().detach().hide()).add(this.footer().detach().hide());
+    }
+    
+    @Override
+    public ListGroup addListGroup() {
+      ListGroup group = super.addListGroup();
+      this.collapse.add(group.detach());
+      return group;
+    }
+    
+    @Override
+    public Table addTable() {
+      Table table = super.addTable();
+      this.collapse.add(table.detach());
+      return table;
+    }
+    
+    public CollapsePanel icon(Icon icon){
+      icon.appendTo(this.anchor);
+      return this;
     }
 
     @Override
