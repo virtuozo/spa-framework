7ce220db03a0ab0b17c74e179a35b5b188bd5b66
mrbraz <anderson.braz@gmail.com>
2015-01-15 07:57:49 -0200

    Method factory

--- commons/src/main/java/virtuozo/infra/LengthValidator.java
+++ commons/src/main/java/virtuozo/infra/LengthValidator.java
@@ -8,13 +8,19 @@ public class LengthValidator extends Validator<LengthValidator, String> {
 
   private Integer max;
 
-  public LengthValidator() {
-    this(0, Integer.MAX_VALUE);
+  public static LengthValidator create(){
+    return new LengthValidator();
+  }
+  
+  private LengthValidator() {
+    this.min = 0;
+    this.max = Integer.MAX_VALUE;
   }
 
-  public LengthValidator(Integer min, Integer max) {
+  public LengthValidator range(Integer min, Integer max) {
     this.min = min;
     this.max = max;
+    return this;
   }
 
   @Override
